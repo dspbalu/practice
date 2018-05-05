e3005d4d2790952c53a8a28793a949f4a23169ad
dspbalu <dspbalu038@gmail.com>
2018-05-05 18:15:38 +0530

    Hanoi

---
+++ TowersOfHanoi.java
@@ -0,0 +1,16 @@
+public class TowersOfHanoi {
+ public static void main(String args[]) {
+  int n = 3;
+  solve(n, 1, 3);
+ }
+ static void solve(int n, int a, int b) {
+  if (n == 1) {
+   System.out.println("Disk " + n + " move from " + a + " to " + b);
+   return;
+  }
+  int helper = 6 - (a + b);
+  solve(n - 1, a, helper);
+  System.out.println("Disk " + n + " move from " + a + " to " + b);
+  solve(n - 1, helper, b);
+ }
+}
